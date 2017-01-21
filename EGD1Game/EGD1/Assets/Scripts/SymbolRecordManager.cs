1bc09be47dbe5bbcedc73485ceeb4138f557ea21
Collin Fingar <collinfingar@gmail.com>
2017-01-21 17:19:04 -0500

    It now shuts off after 3 seconds of non-use

--- EGD1Game/EGD1/Assets/Scripts/SymbolRecordManager.cs
+++ EGD1Game/EGD1/Assets/Scripts/SymbolRecordManager.cs
@@ -9,6 +9,12 @@ public class SymbolRecordManager : MonoBehaviour {
 	public ArrayList newRecording;
 
 	private float timeStartedRecording = 0f;
+	private float timeLastHit = 0f;
+	private float shutOffInterval = 3f;
+
+	public ArrayList savedRecordings = new ArrayList();
+
+
 
 	// Use this for initialization
 	void Start () {
@@ -17,7 +23,9 @@ public class SymbolRecordManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (recording && Time.time > timeLastHit + shutOffInterval) {
+			StopRecording ();
+		}
 	}
 
 	public bool IsRecording(){
@@ -30,6 +38,7 @@ public class SymbolRecordManager : MonoBehaviour {
 			data.Add (shape);
 			data.Add (Time.time - timeStartedRecording);
 			newRecording.Add (data);
+			timeLastHit = Time.time;
 		}
 	}
 
@@ -37,17 +46,24 @@ public class SymbolRecordManager : MonoBehaviour {
 		timeStartedRecording = Time.time;
 		newRecording = new ArrayList ();
 		recording = true;
+		timeLastHit = Time.time;
 		print ("RECORDING");
 	}
 
 	public void StopRecording(){
 		recording = false;
 		print ("STOPPED RECORDING");
-		for (int i = 0; i < newRecording.Count; i++) {
-			ArrayList data = (ArrayList)newRecording [i];
-			string shape = (string)data [0];
-			float time = (float)data [1];
-			Debug.Log (shape + " " + time.ToString ());
+		if (newRecording.Count > 0) {
+			for (int i = 0; i < newRecording.Count; i++) {
+				ArrayList data = (ArrayList)newRecording [i];
+				string shape = (string)data [0];
+				float time = (float)data [1];
+				Debug.Log (shape + " " + time.ToString ());
+			}
+			savedRecordings.Add (newRecording);
+		} else {
+			print ("Nothing");
 		}
+
 	}
 }
