886dbae4559ca9f4d76d15029e3091ef4de7db43
Collin Fingar <collinfingar@gmail.com>
2017-01-21 15:22:03 -0500

    Update

---
+++ EGD1Game/EGD1/Assets/Scripts/TouchSensingScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VR;
+
+public class TouchSensingScript : MonoBehaviour {
+
+	BoxCollider bc;
+
+	// Use this for initialization
+	void Start () {
+		bc = GetComponent<BoxCollider> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other){
+		print (other.name);
+	}
+}
